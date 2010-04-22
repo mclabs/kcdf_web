a2c9a8ba50b9f627276c29399f7e2fe6f6422a4e
jwesonga <johnwesonga@mclabs.it>
2010-04-22 20:41:17 +0300

    added pages-subnva tag

--- kcdf/website/templatetags/subnav_tag.py
+++ kcdf/website/templatetags/subnav_tag.py
@@ -1,6 +1,6 @@
 from django import template
 register = template.Library()
-from kcdf.website.models import Program
+from kcdf.website.models import Program,Page
 
 
 @register.inclusion_tag("website/partials/subnav.html")
@@ -10,3 +10,8 @@ def subnav():
 	return {"programs":programs}
 	
 		
+@register.inclusion_tag("website/partials/pages_subnav.html")
+def pages_subnav(slug):
+	links=[]
+	sub_pages=Page.objects.all().exclude(slug=slug)
+	return {"sub_pages":sub_pages}
