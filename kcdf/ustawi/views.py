98bbe8f064246dd87a70fbde1180ba9a6e695ace
jwesonga <johnwesonga@mclabs.it>
2010-04-28 11:32:25 +0300

    added ustawi and shabaa apps

---
+++ kcdf/ustawi/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render_to_response,get_object_or_404
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+from kcdf.website.models import News
+from django.views.generic import list_detail
+from django.template import RequestContext
+
+def index (request):
+	news=News.objects.all().order_by("created_at")[:4]
+	return render_to_response('ustawi/index.html',{},context_instance=RequestContext(request));
