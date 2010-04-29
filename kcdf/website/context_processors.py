7d38c72a7eaa9e59b332e4a3494ec004f94544f2
jwesonga <johnwesonga@mclabs.it>
2010-04-29 14:30:19 +0300

    modified context_processor to handle patterns

--- kcdf/website/context_processors.py
+++ kcdf/website/context_processors.py
@@ -1,15 +1,17 @@
+import re
 def header_context(request):
     values = {'body_class': "bg-body-home",}
-    if request.path == "/program/shabaa/":
+    #if request.path == "/program/shabaa/":
+    if re.search("/program/shabaa",request.path):
         ## Don't really use an if block, it's just nasty.
         values['body_class'] = "shabaa_masthead"
-    if request.path == "/program/ustawi/":
+    if re.search("/program/ustawi",request.path):
         ## Don't really use an if block, it's just nasty.
         values['body_class'] = "bg-body-inner"
-    if request.path == "/program/arts-culture/":
+    if re.search("/program/arts",request.path):
         ## Don't really use an if block, it's just nasty.
         values['body_class'] = "bg-body-inner"
-    if request.path == "/program/girl-child/":
+    if re.search("/program/girl",request.path):
         ## Don't really use an if block, it's just nasty.
         values['body_class'] = "girl-child_masthead"
 
