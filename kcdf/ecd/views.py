3b7b967c990f14c9935b3e78a50382ed89fa3c87
jwesonga <johnwesonga@mclabs.it>
2010-05-05 00:17:55 +0300

    configured shabaa,ecd,girlchild apps

--- kcdf/ecd/views.py
+++ kcdf/ecd/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.shortcuts import render_to_response,get_object_or_404
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+from kcdf.website.models import *
+from django.views.generic import list_detail
+from django.template import RequestContext
+
+def index (request):
+	news=News.objects.all().order_by("created_at")[:4]
+	return render_to_response('ecd/index.html',{},context_instance=RequestContext(request));
