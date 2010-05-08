389377083f86975dc3985b86ca90352fba898cbd
jwesonga <johnwesonga@mclabs.it>
2010-05-08 20:50:16 +0300

    testing page on ecd

--- kcdf/ecd/views.py
+++ kcdf/ecd/views.py
@@ -18,7 +18,7 @@ def index (request):
 def page (request,slug=""):
 	program=Program.objects.get(slug__contains='childhood')
 	context_dict={'active_tab': 'page',"program":program}
-	return render_to_response('website/page.html',context_dict,context_instance=RequestContext(request));
+	return render_to_response('ecd/page.html',context_dict,context_instance=RequestContext(request));
 
 
 def resources(request):
