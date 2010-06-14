f3dd9299396f26d68096926bdc720e33b36b8029
jwesonga <johnwesonga@mclabs.it>
2010-06-14 11:58:51 +0300

    added arts to the app structure

---
+++ kcdf/arts/urls.py
@@ -0,0 +1,29 @@
+from django.conf.urls.defaults import *
+urlpatterns=patterns('',
+		(r'^search/', include('haystack.urls')),
+
+	)
+
+
+urlpatterns+= patterns('django.contrib.flatpages.views',
+    url(r'^rss/$', 'flatpage', {'url': '/rss/'}, name='rss'),
+    url(r'^donate/$', 'flatpage', {'url': '/donate/'}, name='donate'),
+    url(r'^education-partners/$', 'flatpage', {'url': '/education-partners/'}, name='partners'),
+    url(r'^take-action-education/$', 'flatpage', {'url': '/take-action-education/'}, name='take-action'),
+
+)
+
+urlpatterns+=patterns('arts.views',
+		(r'^$', 'index',{},'home'),
+		(r'^case-studies/$', 'case_studies',{},'case-studies'),
+		(r'^case-study/(?P<slug>[^\.^/]+)/$', 'casestudy_detail',{},'casestudy'),
+		(r'^news/(?P<slug>[^\.^/]+)/$', 'news_detail',{},'news'),
+		(r'^news/$', 'news',{},'news'),
+		(r'^resource/(?P<slug>[^\.^/]+)/$', 'resource_detail',{},'resource'),
+		(r'^events/(?P<slug>[^\.^/]+)/$', 'events_detail',{},'events'),
+		(r'^events/$', 'events',{},'events'),
+		(r'^page/about-arts/$', 'page',{},'about-arts'),
+		(r'^resource-center/$', 'resources',{},'resource-center'),
+
+
+)
---
+++ kcdf/arts/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render_to_response,get_object_or_404
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+from kcdf.website.models import *
+from django.views.generic import list_detail
+from django.template import RequestContext
+
+def index (request):
+	p=Program.objects.get(slug__contains='arts')
+	news=News.objects.filter(program=p).order_by("-created_at")[:4]
+	events=Events.objects.filter(program=p).order_by("-created_at")[:4]
+	cases=CaseStudy.objects.filter(program=p).order_by("-created_at")
+	context_dict={"news":news,"events":events,"cases":cases,"program":p}
+	return render_to_response('arts/index.html',context_dict,context_instance=RequestContext(request));
+
+def page (request,slug=""):
+	program=Program.objects.get(slug__contains='arts')
+	context_dict={'active_tab': 'page',"program":program}
+	return render_to_response('arts/page.html',context_dict,context_instance=RequestContext(request));
+
+
+def resources(request):
+	p=Program.objects.get(slug__contains='arts')
+	resources=Resource.objects.filter(program=p).order_by("-created_at")
+	context_dict={'active_tab': 'resource-center',"resources":resources}
+	return render_to_response('arts/resources.html',context_dict,context_instance=RequestContext(request));
+
+def resource_detail(request,slug):
+	p=Program.objects.get(slug__contains='arts')
+	resources=Resource.filter(program=p).order_by("-created_at")
+	context_dict={'active_tab': 'resource',"resource":resource}
+	return render_to_response('arts/resource_detail.html',context_dict,context_instance=RequestContext(request));
+
+
+def news(request):
+	p=Program.objects.get(slug__contains='arts')
+	news=News.objects.filter(program=p).order_by("-created_at")
+	context_dict={'active_tab': 'news',"news":news}
+	return render_to_response('arts/news.html',context_dict,context_instance=RequestContext(request));
+
+def news_detail(request,slug):
+	news = get_object_or_404(News, slug=slug)
+	context_dict={'active_tab': 'news',"news":news}
+	return render_to_response('arts/news_detail.html',context_dict,context_instance=RequestContext(request));
+
+def events(request):
+	p=Program.objects.get(slug__contains='arts')
+	events=Events.objects.filter(program=p).order_by("-created_at")
+	context_dict={'active_tab': 'events',"events":events}
+	return render_to_response('arts/events.html',context_dict,context_instance=RequestContext(request));
+
+def events_detail(request,slug):
+	events = get_object_or_404(Events, slug=slug)
+	context_dict={'active_tab': 'events',"events":events}
+	return render_to_response('arts/events_detail.html',context_dict,context_instance=RequestContext(request));
+
+
+def case_studies (request):
+	p=Program.objects.get(slug__contains='arts')
+	cases=CaseStudy.objects.filter(program=p).order_by("-created_at")
+	context_dict={'active_tab': 'case-studies',"cases":cases}
+	return render_to_response('arts/casestudies.html',context_dict,context_instance=RequestContext(request));
+
+
+def casestudy_detail(request,slug):
+	casestudy = get_object_or_404(CaseStudy, slug=slug)
+	context_dict={'active_tab': 'case-studies',"casestudy":casestudy}
+	return render_to_response('arts/casestudy_detail.html',context_dict,context_instance=RequestContext(request));
