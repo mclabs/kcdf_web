b60d00e6a50fee082e5c0467f9207900740257fa
jwesonga <johnwesonga@mclabs.it>
2010-06-17 23:23:01 +0300

    added new models to shabaa.models

--- kcdf/shabaa/models.py
+++ kcdf/shabaa/models.py
@@ -10,30 +10,36 @@ class OrganisationCategory(models.Model):
 	def __unicode__ (self):
 		return self.title
 
+	class Meta:
+		verbose_name="Organisation Category"
+		verbose_name_plural="Organisation Categories"
 
 class OrganisationType(models.Model):
 	title=models.CharField(max_length=255)
 
 	def __unicode__ (self):
 		return self.title
+	class Meta:
+		verbose_name="Organisation Type"
+		verbose_name_plural="Organisation Types"
 
 
 class Funder(models.Model):
 	organisation_name=models.CharField(max_length=255)
 	organisation_type=models.ForeignKey(OrganisationType,help_text="Type of Organisation",db_index=True)
 	organisation_category=models.ForeignKey(OrganisationCategory,help_text="Category organisation belongs to",db_index=True)
-	mission=models.TextField()
-	geographical_coverage=models.TextField()
-	eligibility=models.TextField()
-	funding_partners=models.TextField()
-	implementing_partners=models.TextField()
-	physical_address=models.TextField()
-	postal_address=models.TextField()
+	mission=models.TextField(null=True,blank=True)
+	geographical_coverage=models.TextField(null=True,blank=True)
+	eligibility=models.TextField(null=True,blank=True)
+	funding_partners=models.TextField(null=True,blank=True)
+	implementing_partners=models.TextField(null=True,blank=True)
+	physical_address=models.TextField(null=True,blank=True)
+	postal_address=models.TextField(null=True,blank=True)
 	landline=models.CharField(max_length=255)
-	mobile=models.CharField(max_length=255)
-	fax=models.CharField(max_length=100)
-	email=models.CharField(max_length=100)
-	website=models.CharField(max_length=100)
+	mobile=models.CharField(max_length=255,null=True,blank=True)
+	fax=models.CharField(max_length=100,null=True,blank=True)
+	email=models.CharField(max_length=100,null=True,blank=True)
+	website=models.CharField(max_length=100,null=True,blank=True)
 	slug=models.SlugField(max_length=255,unique=True,null=True,blank=True)
 
 	def __unicode__ (self):
@@ -61,16 +67,16 @@ class ServiceType(models.Model):
 class ServiceProvider(models.Model):
 	organisation_name=models.CharField(max_length=255)
 	service_type=models.ForeignKey(ServiceType,help_text="Type of Service Organisation provides",db_index=True)
-	description=models.TextField()
-	funding_partners=models.TextField()
-	implementing_partners=models.TextField()
-	physical_address=models.TextField()
-	postal_address=models.TextField()
-	landline=models.CharField(max_length=255)
-	mobile=models.CharField(max_length=255)
-	fax=models.CharField(max_length=100)
-	email=models.CharField(max_length=100)
-	website=models.CharField(max_length=100)
+	description=models.TextField(null=True,blank=True)
+	funding_partners=models.TextField(null=True,blank=True)
+	implementing_partners=models.TextField(null=True,blank=True)
+	physical_address=models.TextField(null=True,blank=True)
+	postal_address=models.TextField(null=True,blank=True)
+	landline=models.CharField(max_length=255null=True,blank=True)
+	mobile=models.CharField(max_length=255,null=True,blank=True)
+	fax=models.CharField(max_length=100,null=True,blank=True)
+	email=models.CharField(max_length=100,null=True,blank=True)
+	website=models.CharField(max_length=100,null=True,blank=True)
 	slug=models.SlugField(max_length=255,unique=True,null=True,blank=True)
 
 	def __unicode__ (self):
@@ -90,17 +96,17 @@ class ServiceProvider(models.Model):
 
 class LegalDocument(models.Model):
 	title=models.CharField(max_length=255)
-	qualification=models.TextField()
-	requirements=models.TextField()
-	fee=models.TextField()
-	application_place=models.TextField()
-	head_office=models.TextField()
-	postal_address=models.TextField()
-	landline=models.CharField(max_length=255)
-	mobile=models.CharField(max_length=255)
-	fax=models.CharField(max_length=100)
-	email=models.CharField(max_length=100)
-	website=models.CharField(max_length=100)
+	qualification=models.TextField(null=True,blank=True)
+	requirements=models.TextField(null=True,blank=True)
+	fee=models.TextField(null=True,blank=True)
+	application_place=models.TextField(null=True,blank=True)
+	head_office=models.TextField(null=True,blank=True)
+	postal_address=models.TextField(null=True,blank=True)
+	landline=models.CharField(max_length=255,null=True,blank=True)
+	mobile=models.CharField(max_length=255,null=True,blank=True)
+	fax=models.CharField(max_length=100,null=True,blank=True)
+	email=models.CharField(max_length=100,null=True,blank=True)
+	website=models.CharField(max_length=100,null=True,blank=True)
 	slug=models.SlugField(max_length=255,unique=True,null=True,blank=True)
 
 	def __unicode__ (self):
@@ -116,6 +122,48 @@ class LegalDocument(models.Model):
 	def save (self):
 		self.slug = slugify(self.title)
 		super(LegalDocument,self).save()
+
+
+class BusinessRegistration(models.Model):
+	business_type=models.CharField(max_length=255)
+	requirements=models.TextField(null=True,blank=True)
+	frequency=models.CharField(max_length=25,help_text="How many times you have to apply for the license",null=True,blank=True)
+	organisation=models.CharField(max_length=255,help_text="Organisation that facilitates this business registration",null=True,blank=True)
+	slug=models.SlugField(max_length=255,unique=True,null=True,blank=True)
+	
+	def __unicode__ (self):
+		return self.business_type
+	
+	class Meta:
+		verbose_name="Business Registration"
+		verbose_name_plural="Business Registrations"
+		
+	def get_absolute_url(self):
+		return "/business_registration/%s/" % self.slug
+
+	def save (self):
+		self.slug = slugify(self.business_type)
+		super(BusinessRegistration,self).save()
+
+
+class IndividualRegistration(models.Model):
+	organisation_name=models.CharField(max_length=255)
+	description=models.TextField(null=True,blank=True)
+	requirements=models.TextField(null=True,blank=True)
+	cost=models.CharField(max_length=100,null=True,blank=True)
+	physical_address=models.TextField(null=True,blank=True)
+	slug=models.SlugField(max_length=255,unique=True,null=True,blank=True)
+
+	def __unicode__ (self):
+		return self.organisation_name
+	
+	class Meta:
+		verbose_name="Individual Registration"
+		verbose_name_plural="Individual Registrations"
+
+	def save (self):
+		self.slug = slugify(self.organisation_name)
+		super(IndividualRegistration,self).save()
 
 
 '''
