0ae6f85373ed08d8849eac32443633751d663872
John Wesonga <johnwesonga@gmail.com>
2010-06-29 18:50:56 +0300

    changed ustawi views to use food security

--- kcdf/ustawi/views.py
+++ kcdf/ustawi/views.py
@@ -8,7 +8,7 @@ from django.views.generic import list_detail
 from django.template import RequestContext
 
 def index (request):
-	p=Program.objects.get(slug__contains='ustawi')
+	p=Program.objects.get(slug__contains='food-security')
 	news=News.objects.filter(program=p).order_by("-created_at")[:4]
 	events=Events.objects.filter(program=p).order_by("-created_at")[:4]
 	cases=CaseStudy.objects.filter(program=p).order_by("-created_at")
@@ -16,13 +16,13 @@ def index (request):
 	return render_to_response('ustawi/index.html',context_dict,context_instance=RequestContext(request));
 
 def page (request,slug=""):
-	program=Program.objects.get(slug__contains='ustawi')
+	program=Program.objects.get(slug__contains='food-security')
 	context_dict={'active_tab': 'page',"program":program}
 	return render_to_response('ustawi/page.html',context_dict,context_instance=RequestContext(request));
 
 
 def resources(request):
-	p=Program.objects.get(slug__contains='ustawi')
+	p=Program.objects.get(slug__contains='food-security')
 	resources=Resource.objects.filter(program=p).order_by("-created_at")
 	context_dict={'active_tab': 'resource-center',"resources":resources}
 	return render_to_response('ustawi/resources.html',context_dict,context_instance=RequestContext(request));
@@ -34,7 +34,7 @@ def resource_detail(request,slug):
 
 
 def news(request):
-	p=Program.objects.get(slug__contains='ustawi')
+	p=Program.objects.get(slug__contains='food-security')
 	news=News.objects.filter(program=p).order_by("-created_at")
 	context_dict={'active_tab': 'news',"news":news}
 	return render_to_response('ustawi/news.html',context_dict,context_instance=RequestContext(request));
@@ -58,7 +58,7 @@ def events_detail(request,slug):
 
 
 def case_studies (request):
-	p=Program.objects.get(slug__contains='ustawi')
+	p=Program.objects.get(slug__contains='food-security')
 	cases=CaseStudy.objects.filter(program=p).order_by("-created_at")
 	context_dict={'active_tab': 'case-studies',"cases":cases}
 	return render_to_response('ustawi/casestudies.html',context_dict,context_instance=RequestContext(request));
