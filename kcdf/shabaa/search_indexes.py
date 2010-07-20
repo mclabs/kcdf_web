b31561f2cdec27a3fd3725e0014cd70930540d57
jwesonga <johnwesonga@mclabs.it>
2010-07-20 10:02:43 +0300

    fixed bug in shabaa search_indexes

--- kcdf/shabaa/search_indexes.py
+++ kcdf/shabaa/search_indexes.py
@@ -5,6 +5,6 @@ from shabaa.models import *
 class FunderIndex(SearchIndex):
     text = CharField(document=True, use_template=True)
     organisation_name = CharField(model_attr='organisation_name')
-    content = CharField(model_attr='organisation_mission')
+    content = CharField(model_attr='mission')
 
 site.register(Funder, FunderIndex)
