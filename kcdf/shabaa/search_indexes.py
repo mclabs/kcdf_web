6a79c40918f43f2f47281a5fc9c782aa05192288
jwesonga <johnwesonga@mclabs.it>
2010-07-20 11:20:27 +0300

    added a search index for shabaa app

--- kcdf/shabaa/search_indexes.py
+++ kcdf/shabaa/search_indexes.py
@@ -7,4 +7,19 @@ class FunderIndex(SearchIndex):
     organisation_name = CharField(model_attr='organisation_name')
     content = CharField(model_attr='mission')
 
+class ServiceProviderIndex(SearchIndex):
+    text = CharField(document=True, use_template=True)
+    organisation_name = CharField(model_attr='organisation_name')
+    content = CharField(model_attr='description')
+    funding_partners = CharField(model_attr='funding_partners')
+
+class LegalDocumentIndex(SearchIndex):
+    text = CharField(document=True, use_template=True)
+    title = CharField(model_attr='title')
+    content = CharField(model_attr='qualification')
+    requirements = CharField(model_attr='requirements')
+
+
 site.register(Funder, FunderIndex)
+site.register(ServiceProvider, ServiceProviderIndex)
+site.register(LegalDocument, LegalDocumentIndex)
