994efa43aac5d23c9a210cb37de827527d7b4b02
jwesonga <johnwesonga@mclabs.it>
2010-07-20 09:48:50 +0300

    added a search index for shabaa app

---
+++ kcdf/shabaa/search_indexes.py
@@ -0,0 +1,10 @@
+from haystack.indexes import *
+from haystack import site
+from shabaa.models import *
+
+class FunderIndex(SearchIndex):
+    text = CharField(document=True, use_template=True)
+    organisation_name = CharField(model_attr='organisation_name')
+    content = CharField(model_attr='organisation_mission')
+
+site.register(Funder, FunderIndex)
