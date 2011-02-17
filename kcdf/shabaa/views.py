12b72d62f45964cabd1f9e75c56f584ff2b4a815
jwesonga <johnwesonga@mclabs.it>
2011-02-17 19:30:48 +0300

    modified pdf method

--- kcdf/shabaa/views.py
+++ kcdf/shabaa/views.py
@@ -160,7 +160,7 @@ def print_pdf(request,slug):
 	response['Content-Disposition']='attachment;filename=%s'%(slug)
 	c=canvas.Canvas(response)
 	sample_text=strip_tags(funder.eligibility)
-	text_width = stringWidth(sample_text,"","")
+	text_width = stringWidth(sample_text,"Helvetica",14)
 	y = 1050
 	text=c.beginText((PAGE_WIDTH - text_width) / 2.0, y)
 	text.setTextOrigin(inch,2.5*inch)
