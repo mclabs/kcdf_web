2268d850ad3adba9293a4d81532b54313b1a17cd
jwesonga <johnwesonga@mclabs.it>
2011-03-09 22:52:39 +0300

    added feedparsed

--- kcdf/website/templatetags/tags.py
+++ kcdf/website/templatetags/tags.py
@@ -3,6 +3,7 @@ from django.template import loader, Node, Variable
 from django.utils.encoding import smart_str, smart_unicode
 from django.template.defaulttags import url
 from django.template import VariableDoesNotExist
+import datetime
 
 register = template.Library()
 @register.simple_tag
@@ -11,3 +12,9 @@ def active(request, pattern):
     if re.search(pattern, request.path):
         return 'active'
     return ''
+
+register = template.Library()
+def feedparsed(value):
+	return datetime.datetime(*value[:6])
+register.filter(feedparsed)
+	
